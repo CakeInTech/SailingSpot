f33958a077729fa78f2677b4acfa6975e712a9e9
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-05 17:03:42 +0200

    - Run db create

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_143553) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_144100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boats", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "photo"
+    t.decimal "price"
+    t.text "model"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boats_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "city"
+    t.date "pick_up"
+    t.date "return_date"
+    t.bigint "user_id", null: false
+    t.bigint "boat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_reservations_on_boat_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_143553) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "boats", "users"
+  add_foreign_key "reservations", "boats"
+  add_foreign_key "reservations", "users"
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/api/v1/users/new"
+RSpec.describe 'Api::V1::Users', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/api/v1/users/new'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/users/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/api/v1/users/create'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
