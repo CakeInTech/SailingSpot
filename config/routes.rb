9bccb99c6a45b6b4d68abe7a0fdc379e610a6a0d
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-05-05 10:21:32 +0200

    - Generate User controller and render json data

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "root#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
