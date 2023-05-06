91766edac13ed8bb3eeaa99ba674deb9c7cbc626
developertariq <dev.tariqul.islam@gmail.com>
2023-05-06 19:14:36 +0600

    Customize routes.rb for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api/v1'
+  mount Rswag::Api::Engine => '/api/v1'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
