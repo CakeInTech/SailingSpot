3bb29370629a7a1b2e6e6382e9358ca39f0931b0
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-05-08 12:53:25 +0200

    - Fix rubocop errors

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :request do
   describe 'GET #index' do
     it 'returns a list of users' do
-      user = User.create(name: 'John')
+      User.create(name: 'John')
       get '/api/v1/users'
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)['users'].first['name']).to eq('John')
