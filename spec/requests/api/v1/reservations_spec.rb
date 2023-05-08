3eff3afef10d48afa4346cbb67f99e9c949bf4d9
developertariq <dev.tariqul.islam@gmail.com>
2023-05-08 21:55:30 +0600

    Update reservation test cases according to route

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Users', type: :request do
   describe 'GET /new' do
     it 'returns http success' do
-      get '/api/v1/users/1/boats/1/reservations'
+      get '/api/v1/users/1/reservations'
       expect(response).to have_http_status(:success)
     end
   end
