b59f90f024f8817321b91f1d1408742d9bcc8a35
Newton <altontonnalumasa@gmail.com>
2023-05-08 13:04:14 +0300

    Write test to reservation

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -1,9 +1,10 @@
-# require 'rails_helper'
-# RSpec.describe "Api::V1::Users", type: :request do
-#   describe "GET /new" do
-#     it "returns http success" do
-#       get "/api/v1/users/1/boats/1/reservations"
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Users', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/api/v1/users/1/boats/1/reservations'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
