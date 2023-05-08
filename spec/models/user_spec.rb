ef6baf592568b74629abe23f65c8e7c592146ea0
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-05-08 11:34:27 +0200

    - Create an rspec test for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'requires name to be present' do
+      user = User.new(name: nil)
+      expect(user).not_to be_valid
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+  end
 end
