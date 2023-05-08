f425b91c97959fd5be3b3fc0744cfc4349e5e790
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-05-08 11:36:41 +0200

    - Create a spec test for users controller

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,17 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Api::V1::Users', type: :request do
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/api/v1/users/new'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/api/v1/users/create'
-      expect(response).to have_http_status(:success)
+RSpec.describe Api::V1::UsersController, type: :request do
+  describe 'GET #index' do
+    it 'returns a list of users' do
+      user = User.create(name: 'John')
+      get '/api/v1/users'
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['users'].first['name']).to eq('John')
     end
   end
 end
