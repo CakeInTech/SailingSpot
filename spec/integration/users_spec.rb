32909e9d9afeede4bdba7f1665f4b6e86ee2b94c
developertariq <dev.tariqul.islam@gmail.com>
2023-05-10 02:05:22 +0600

    Write test cases for users

--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -50,26 +50,6 @@ RSpec.describe 'api/v1/users', type: :request do
     end
   end
 
-  path '/api/v1/users/{id}/edit' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-
-    get('edit user') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
   path '/api/v1/users/{id}' do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
@@ -105,21 +85,6 @@ RSpec.describe 'api/v1/users', type: :request do
     end
 
     put('update user') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    delete('delete user') do
       response(200, 'successful') do
         let(:id) { '123' }
 
