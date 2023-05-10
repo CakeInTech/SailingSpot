3471de08d8e488c093cb971a76e282e72a7969c2
developertariq <dev.tariqul.islam@gmail.com>
2023-05-11 02:47:27 +0600

    Update application.js file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,11 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/app";
-
+import App from "./components/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
   <App/>,
