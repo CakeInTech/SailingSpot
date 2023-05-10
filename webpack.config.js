37b2daddc1a666921b61efd1288ad185e314e243
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-05-10 09:38:13 +0200

    - Include style and css loaders in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,12 @@ module.exports = {
   entry: {
     application: "./app/javascript/application.js"
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      usedExports: true,
+    },
+  },
   module: {
     rules: [
       {
@@ -14,16 +20,24 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
+    path: path.resolve(__dirname, 'app/assets/builds'),
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+      maxChunks: 1,
+    }),
+  ],
+};
