1d57e4c161d539f5a3e8d0dfe320233b76d02cc7
developertariq <dev.tariqul.islam@gmail.com>
2023-05-11 02:46:54 +0600

    Implement router to navigate page

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import HomePage from '../pages/Homepage';
+import Reserve from '../pages/Reserve';
+import MyReservations from '../pages/MyReservations';
+import AddBoat from '../pages/AddBoat'
+import DeleteBoat from '../pages/DeleteBoat';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
+        <Router>
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/reserve" element={<Reserve />} />
+            <Route path="/my-reservations" element={<MyReservations />} />
+            <Route path="/add-boat" element={<AddBoat />} />
+            <Route path="/delete-boat" element={<DeleteBoat />} />
+          </Routes>
+        </Router>
     )
 }
 
