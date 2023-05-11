7442e71a792904f3b3b6fb4f8d7db011d7ac877c
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-11 15:56:25 +0200

    Add sign in and login entry points

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,19 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  devise_for :users, path: '', path_names: {
+  sign_in: 'login',
+  sign_out: 'logout',
+  registration: 'signup'
+}
   root "root#index"
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show] do
         resources :boats, only: [:index, :show]
         resources :reservations, only: [:index]
-      end
     end
   end
+  get '*path', to: 'root#index', via: :all
 end
