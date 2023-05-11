7aa8385f78103b9a0c48dcc94ec70978aa11798a
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-11 16:02:13 +0200

    Update app.js with routes to each component

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import Welcome from './welcome';
 
-
-const App = () => {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    )
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/welcome" element={<Welcome/>} />
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
