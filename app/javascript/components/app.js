88176e9f65b71fcfc62f42eb2abf424c2ee4b2d0
developertariq <dev.tariqul.islam@gmail.com>
2023-05-11 18:27:39 +0600

    Implement store

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../Redux/configureStore'
 import Sidebar from './Sidebar';
 import HomePage from '../pages/Homepage';
 import Reserve from '../pages/Reserve';
@@ -8,18 +10,23 @@ import AddBoat from '../pages/AddBoat'
 import DeleteBoat from '../pages/DeleteBoat';
 
 const App = () => {
-    return (
-        <Router>
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/reserve" element={<Reserve />} />
-            <Route path="/my-reservations" element={<MyReservations />} />
-            <Route path="/add-boat" element={<AddBoat />} />
-            <Route path="/delete-boat" element={<DeleteBoat />} />
-          </Routes>
-        </Router>
-    )
+  return (
+    <Provider store={store}>
+      
+      <Router>
+        <div className='container'>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/reserve" element={<Reserve />} />
+          <Route path="/my-reservations" element={<MyReservations />} />
+          <Route path="/add-boat" element={<AddBoat />} />
+          <Route path="/delete-boat" element={<DeleteBoat />} />
+        </Routes>
+        </div>
+      </Router>
+    </Provider>
+  )
 }
 
 export default App;
