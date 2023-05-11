8b569f4209430964dffdfa289b75eb63539a2ccc
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-11 16:01:11 +0200

    Add sign up component

---
+++ app/javascript/components/Signup.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import userslice, { registerUser, userSelector } from '../redux/userslice';
+
+const SignUp = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch()
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const {
+      name, email, password, password_confirmation
+    } = state
+
+    dispatch(
+      registerUser({
+        name, email, password, password_confirmation
+      })
+    )
+    navigate('/welcome')
+  }
+
+  const user = useSelector(userSelector)
+  console.log(user)
+
+  
+
+  
+
+  // useEffect(() => {
+  //   if(success) {
+  //     
+  //   }
+  // })
+
+
+  // const user = useSelector(state => state.signup.signupResponse);
+  // console.log(user)
+  // const dispatch = useDispatch();
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [password_confirmation, setPasswordConfirmation] = useState('');
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+  //   axios.post('/signup', { user: { name, email, password, password_confirmation }})
+    
+  //     .then((response) => {
+  //       const { user } = response.data;
+  //       dispatch({ type: 'SET_SIGNUP_RESPONSE', payload: user });
+  //       navigate('/welcome');
+  //     })
+  //     .catch((error) => {
+  //       setError(error.response.data.errors.full_messages.join(', '));
+  //     });
+  // };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" value={state.name} onChange={handleInputChange} />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" value={state.email} onChange={handleInputChange} />
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" value={state.password} onChange={handleInputChange} />
+      </label>
+      <label>
+        Re-Password:
+        <input type="password" name="password_confirmation" value={state.password_confirmation} onChange={handleInputChange} />
+      </label>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
+
+export default SignUp;
