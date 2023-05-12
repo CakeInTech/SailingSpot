2a52d34643e500bbe68d93ef6d5421bc9335ce50
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-05-12 16:12:08 +0200

    - Wrap Route components with home class

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import HomePage from './Homepage';
 import Reserve from './Reserve';
 import MyReservations from './MyReservations';
 import AddBoat from './AddBoat'
 import DeleteBoat from './DeleteBoat';
+import "../scss/app.scss"
 
 const App = () => {
-    return (
-        <Router>
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/reserve" element={<Reserve />} />
-            <Route path="/my-reservations" element={<MyReservations />} />
-            <Route path="/add-boat" element={<AddBoat />} />
-            <Route path="/delete-boat" element={<DeleteBoat />} />
-          </Routes>
-        </Router>
-    )
-}
+  return (
+    <>
+        <Sidebar />
+        <div className='home'>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/reserve" element={<Reserve />} />
+              <Route path="/my-reservations" element={<MyReservations />} />
+              <Route path="/add-boat" element={<AddBoat />} />
+              <Route path="/delete-boat" element={<DeleteBoat />} />
+            </Routes>
+        </div>
+      </>
+  );
+};
 
 export default App;
