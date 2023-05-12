a51853ad5707aa22f1c80d8300d3e694bf451cc5
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-12 18:03:01 +0200

    Add logic to sign in and persist user data

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -1,46 +1,66 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../redux/userslice";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    axios.post('/login', { email, password })
-      .then((response) => {
-        // Handle successful login
-        console.log(response.data);
-      })
-      .catch((error) => {
-        // Handle login error
-        setError(error.response.data.error);
-      });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = formData?.email;
+    const password = formData?.password;
+  
+    dispatch(
+      loginUser({
+        email,
+        password,
+      })
+    );
+    navigate("/welcome");
+  };
+
+  
+
   return (
+    <div>
+    <h1>Login</h1>
     <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-      </label>
-      {error && <p>{error}</p>}
-      <button type="submit">Log in</button>
-      <div>
-        <h1>Login</h1>
-        <p>Don't have account? <Link to="/signup">Sign up here.</Link></p>
-       </div>
+    <label>
+    Email:
+    <input
+             type="email"
+             name="email"
+             value={formData.email}
+             onChange={handleChange}
+             required
+           />
+    </label>
+    <br />
+    <label>
+    Password:
+    <input
+             type="password"
+             name="password"
+             value={formData.password}
+             onChange={handleChange}
+             required
+           />
+    </label>
+    <br />
+    <button type="submit">Submit</button>
     </form>
-  );
-};
-
-export default Login;
-
-
+    <p>
+    Don't have an account? <Link to="/signup">Sign up here.</Link>
+    </p>
+    </div>
+    );
+    };
+    
+    export default Login;
--- app/javascript/components/welcome.js
+++ app/javascript/components/welcome.js
@@ -1,29 +1,52 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { userSelector } from '../redux/userslice';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout, userSelector } from '../redux/userslice';
 
 const Welcome = () => {
-    const user = useSelector(userSelector)
+  const user = useSelector(userSelector)
   console.log(user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate();
+
+
+  const handleLogout = async () => {
+    try {
+      await fetch("/logout", { method: "DELETE" });
+      dispatch(logout());
+      navigate('/signup')
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
-      {user && (
+      {user.success && (
         <>
           <h1>Welcome, {user.name}!</h1>
           <p>Your email address is {user.email}</p>
           <p>You have successfully signed up.</p>
+          <button onClick={handleLogout}>Logout</button>
         </>
       )}
-      <div>
-        <h1>signup</h1>
-        <p>Don't have account? <Link to="/signup">Sign up here.</Link></p>
-      </div>
-      <div>
-        <h1>Login</h1>
-        <p>have account? <Link to="/login">login up here.</Link></p>
-      </div>
+
+       {!user.success && (
+        <>
+          <div>
+            <h1>signup</h1>
+            <p>
+              Don't have an account? <Link to="/signup">Sign up here.</Link>
+            </p>
+          </div>
+          <div>
+            <h1>Login</h1>
+            <p>
+              Have an account? <Link to="/login">Login up here.</Link>
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
   
