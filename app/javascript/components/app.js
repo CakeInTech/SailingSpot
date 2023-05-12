f3745be863e5fcdae58b9843545221c2f71c5f7a
Newton <altontonnalumasa@gmail.com>
2023-05-12 13:50:41 +0300

    Remove app file

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { Route, Routes } from 'react-router-dom'
-import Homepage from "./Homepage";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../Redux/configureStore'
+import Sidebar from './Sidebar';
+import HomePage from '../pages/Homepage';
+import Reserve from '../pages/Reserve';
+import MyReservations from '../pages/MyReservations';
+import AddBoat from '../pages/AddBoat'
+import DeleteBoat from '../pages/DeleteBoat';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/reservation" element={<Homepage />} />
-    </Routes>
+    <Provider store={store}>
+      
+      <Router>
+        <div className='container'>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/reserve" element={<Reserve />} />
+          <Route path="/my-reservations" element={<MyReservations />} />
+          <Route path="/add-boat" element={<AddBoat />} />
+          <Route path="/delete-boat" element={<DeleteBoat />} />
+        </Routes>
+        </div>
+      </Router>
+    </Provider>
   )
 }
 
