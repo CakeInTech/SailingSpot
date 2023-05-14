c8bf170f206b61d12790bc103efa180930e15828
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-14 12:45:41 +0200

    Add styling to sign up page

--- app/javascript/components/Signup.js
+++ app/javascript/components/Signup.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import userslice, { registerUser, userSelector } from '../redux/userslice';
+import '../scss/signup.scss';
+
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -32,31 +34,39 @@ const SignUp = () => {
       registerUser({
         name, email, password, password_confirmation
       })
-    )
-    navigate('/welcome')
+    ).then(() => {
+      navigate('/');
+    }).catch(error => {
+      const err = ('Registration failed with error:', error);
+    });
   }
 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" name="name" value={state.name} onChange={handleInputChange} />
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" value={state.email} onChange={handleInputChange} />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" value={state.password} onChange={handleInputChange} />
-      </label>
-      <label>
-        Re-Password:
-        <input type="password" name="password_confirmation" value={state.password_confirmation} onChange={handleInputChange} />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className='signup-container'>
+      <h1>Create Account</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={state.name} onChange={handleInputChange} required />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" value={state.email} onChange={handleInputChange} required/>
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" value={state.password} onChange={handleInputChange} required />
+        </label>
+        <label>
+          Re-Password:
+          <input type="password" name="password_confirmation" value={state.password_confirmation} onChange={handleInputChange} required />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+      <p>Already have an account?  <Link to="/login">Login up here</Link></p>
+
+    </div>
   );
 }
 
