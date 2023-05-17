178989089595bc281450da35aa07c6b0aff4310a
Newton <altontonnalumasa@gmail.com>
2023-05-17 13:17:31 +0300

    Add radio button for availability

--- app/javascript/components/AddBoat.js
+++ app/javascript/components/AddBoat.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addBoat } from "../Redux/Boats/addBoat";
 
 const AddBoat = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const { user } = useSelector((state) => state.user);
   const [photo, setPhoto] = useState("");
   const [price, setPrice] = useState("");
   const [model, setModel] = useState("");
+  const [availability, setAvailability] = useState("");
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,15 +18,18 @@ const AddBoat = () => {
       description.trim() &&
       photo.trim() &&
       price.trim() &&
-      model.trim()
+      model.trim() &&
+      availability.trim()
     ) {
       dispatch(
-        addBoat({
+        addBoat(user.id, {
           name,
           description,
           photo,
           price,
           model,
+          availability,
+          user_id: user.id,
         })
       );
       setName("");
@@ -32,14 +37,15 @@ const AddBoat = () => {
       setPhoto("");
       setPrice("");
       setModel("");
+      setAvailability("");
     }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <h1 className="text-center mt-5 border border-primary">Add a Boat</h1>
+    <div>
+      <h1 className="text-center mt-5 border border-primary">Add a Boat</h1>
+      <form class="w-50 mx-auto was-validated" onSubmit={handleSubmit}>
         <div className="form-group m-4">
-          <label for="city">Enter Boat Name</label>
+          <label for="name">Enter Boat Name</label>
           <input
             type="text"
             className="form-control"
@@ -54,7 +60,7 @@ const AddBoat = () => {
           <div class="invalid-feedback">Please fill out this field.</div>
         </div>
         <div className="form-group m-4">
-          <label for="city">Enter Description</label>
+          <label for="description">Enter Description</label>
           <input
             type="text"
             className="form-control"
@@ -69,7 +75,7 @@ const AddBoat = () => {
           <div class="invalid-feedback">Please fill out this field.</div>
         </div>
         <div className="form-group m-4">
-          <label for="city">Enter Photo</label>
+          <label for="photo">Enter Photo</label>
           <input
             type="string"
             className="form-control"
@@ -84,22 +90,7 @@ const AddBoat = () => {
           <div class="invalid-feedback">Please fill out this field.</div>
         </div>
         <div className="form-group m-4">
-          <label for="city">Enter Model</label>
-          <input
-            type="text"
-            className="form-control"
-            id="model"
-            placeholder="model"
-            name="model"
-            value={model}
-            onChange={(e) => setModel(e.target.value)}
-            required
-          />
-          <div class="valid-feedback">Valid.</div>
-          <div class="invalid-feedback">Please fill out this field.</div>
-        </div>
-        <div className="form-group m-4">
-          <label for="city">Enter Model</label>
+          <label for="price">Enter Price</label>
           <input
             type="decimal"
             className="form-control"
@@ -113,11 +104,53 @@ const AddBoat = () => {
           <div class="valid-feedback">Valid.</div>
           <div class="invalid-feedback">Please fill out this field.</div>
         </div>
-        <button type="submit" className="input-submit">
+        <div className="form-group m-4">
+          <label for="model">Enter Model</label>
+          <input
+            type="text"
+            className="form-control"
+            id="model"
+            placeholder="model"
+            name="model"
+            value={model}
+            onChange={(e) => setModel(e.target.value)}
+            required
+          />
+          <div class="valid-feedback">Valid.</div>
+          <div class="invalid-feedback">Please fill out this field.</div>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="availability"
+            id="availability"
+            value={availability}
+            onChange={(e) => setAvailability(e.target.value)}
+          />
+          <label class="form-check-label" for="flexRadioDefault1">
+            Availble
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="availability"
+            id="availability"
+            value={availability}
+            onChange={(e) => setAvailability(e.target.value)}
+            checked
+          />
+          <label class="form-check-label" for="flexRadioDefault2">
+            Not Available
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary mt-4">
           Add Boat
         </button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
