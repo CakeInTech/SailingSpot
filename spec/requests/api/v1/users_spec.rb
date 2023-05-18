6182e77096b0736002f648cb4dea376f628d52c6
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-05-18 11:15:19 +0200

    Fix failing  spec test

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :request do
   describe 'GET #index' do
     it 'returns a list of users' do
-      User.create(name: 'Newton', email: 'altontonnalumasa@gmail.com', password: '123456')
+      User.create(name: 'Chimezie Asouzu', email: 'altontonnalumasa@gmail.com', password: '123456')
       get '/api/v1/users'
       expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)['users'].first['name']).to eq('Newton')
+      expect(JSON.parse(response.body)['users'].first['name']).to eq('Chimezie Asouzu')
     end
   end
 end
