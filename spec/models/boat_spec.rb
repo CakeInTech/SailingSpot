3b4ea0cb28f21c6015fdc1181b16f6dc7c5e97e8
Newton <altontonnalumasa@gmail.com>
2023-05-18 10:52:33 +0300

    Write test for models

--- spec/models/boat_spec.rb
+++ spec/models/boat_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe Boat, type: :model do
     it { should validate_presence_of(:price) }
     it { should validate_numericality_of(:price).is_greater_than(0) }
     it { should validate_presence_of(:photo) }
-    it { should validate_uniqueness_of(:photo) }
     it { should belong_to(:user) }
   end
 end
